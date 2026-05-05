6104d71f81978a9078c483f0a1daa84f01ffadf4
mohamedmoeen <mohamed.moeen166@gmail.com>
2026-05-06 02:27:22 +0300

    implement consumer and response time

---
+++ src/main/java/org/example/ConsumerClass.java
@@ -0,0 +1,57 @@
+package org.example;
+
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Properties;
+
+public class ConsumerClass {
+    public static void main() {
+        Properties props = new Properties();
+        props.put("bootstrap.servers","localhost:9092");
+        props.put("key.deserializer",
+                "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer",
+                "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("group.id","group1");
+        props.put("auto.offset.reset", "earliest");
+        Consumer<String,String> consumer = new KafkaConsumer<>(props);
+        List<String> list = new ArrayList<>();
+        list.add("barcelona");
+        consumer.subscribe(list);
+        System.out.println("Consumer subuscribedddd");
+
+        int rec = 0;
+        List<Long> responses = new ArrayList<>();
+        while(rec<1000){
+            long start = System.currentTimeMillis();
+
+            ConsumerRecords<String,String> records = consumer.poll(Duration.ofMillis(10));
+            if(!records.isEmpty()){
+                long responseTimeInMillis = System.currentTimeMillis() - start;
+                responses.add(responseTimeInMillis);
+            }
+            for(ConsumerRecord<String,String> record: records){
+                rec++;
+                System.out.println(
+                        "Topic: " + record.topic() +
+                                " | Partition: " + record.partition() +
+                                " | Offset: " + record.offset()
+                );
+
+                System.out.println("Message:\n" + record.value());
+            }
+        }
+        Collections.sort(responses);
+        System.out.println("Median is "+responses.get(responses.size()/2));
+
+
+    }
+}
---
+++ src/main/java/org/example/ProducerClass.java
@@ -0,0 +1,44 @@
+package org.example;
+
+import org.apache.kafka.clients.producer.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.concurrent.ExecutionException;
+
+public class ProducerClass {
+    public static void main() throws IOException, ExecutionException, InterruptedException {
+        String content = Files.readString(Paths.get("src/main/resources/message.txt"));
+//        System.out.println(content);
+        Properties props = new Properties();
+        props.put("bootstrap.servers","localhost:9092");
+        props.put("key.serializer",
+                "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer",
+                "org.apache.kafka.common.serialization.StringSerializer");
+        Producer<String, String> producer = new KafkaProducer<>(props);
+        ProducerRecord<String,String>record = new ProducerRecord<>("barcelona",content);
+        List<Long> responses = new ArrayList<>();
+        for(int i = 0;i<1000;i++){
+            long start = System.currentTimeMillis();
+            producer.send(record,((metadata, exception) ->{
+                if(exception == null){
+                    System.out.println("producer sent to topic "+metadata.topic() + "partition "+metadata.partition());
+                }
+                else {
+                    exception.printStackTrace();
+                }
+            } )).get();
+            long responseTimeInMillis = System.currentTimeMillis() - start;
+            responses.add(responseTimeInMillis);
+        }
+        Collections.sort(responses);
+        System.out.println("Median is "+responses.get(499));
+
+
+
+        producer.close();
+    }
+}
